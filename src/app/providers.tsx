b53eb6586407eeffbc46a693c73cac4b88659a39
Cody Crozier <wccrozier12@gmail.com>
2025-05-16 11:16:47 -0700

    fix import

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,8 +3,9 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { type ReactNode, useState } from 'react'
 import { type State, WagmiProvider } from 'wagmi'
+import { getConfig } from '../wagmi'
 
-import { getConfig } from '@/wagmi'
+
 
 export function Providers(props: {
   children: ReactNode
