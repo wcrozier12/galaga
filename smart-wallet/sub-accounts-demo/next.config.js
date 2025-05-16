dd864b5634748ad944ec738c9115c343e054c90e
Cody Crozier <wccrozier12@gmail.com>
2025-05-16 10:57:52 -0700

    ignore

--- smart-wallet/sub-accounts-demo/next.config.js
+++ smart-wallet/sub-accounts-demo/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    ignoreBuildErrors: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+      },
+      typescript: {
+        ignoreBuildErrors: true,
+      },
 }
 
 module.exports = nextConfig
